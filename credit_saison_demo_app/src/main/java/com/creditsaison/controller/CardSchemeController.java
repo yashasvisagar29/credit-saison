42a8c8a51eebedc2a59d965909c0019cc3e2eafe
Yashasvi.Sagar <Yashasvi.Sagar@INC01007--.Incture.local>
2021-06-02 08:58:12 +0530

    added ui for app

--- credit_saison_demo_app/src/main/java/com/creditsaison/controller/CardSchemeController.java
+++ credit_saison_demo_app/src/main/java/com/creditsaison/controller/CardSchemeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +20,14 @@ import com.creditsaison.util.AppErrorMsgConstants;
 import com.creditsaison.util.HelperClass;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/card-scheme")
 public class CardSchemeController {
 
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Autowired 
+	@Autowired
 	private NumberOfHitsOnCardService services;
 
 	@GetMapping("/lookup/{cardNum}")
